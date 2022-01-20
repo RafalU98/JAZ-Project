76d05629a07e519cd545256160b155f5cab56b34
RafalU98 <73042642+RafalU98@users.noreply.github.com>
2022-01-20 17:59:43 +0100

    Created a table + a controller

---
+++ src/test/java/pl/edu/pjwstk/JazProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pjwstk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
